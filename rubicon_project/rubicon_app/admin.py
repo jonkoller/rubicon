d821515de4e68c57457b152a89b6ab2c8b91cb39
Jonathan Koller <koller3@gmail.com>
2019-02-12 11:40:13 -0600

    Add Player model
     - this is the standard django user "Profile" way of extending the "User" model

--- rubicon_project/rubicon_app/admin.py
+++ rubicon_project/rubicon_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import Player
+
+class PlayerInline(admin.StackedInline):
+    model = Player
+    can_delete = False
+    verbose_name_plural = 'player'
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (PlayerInline,)
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- rubicon_project/rubicon_app/models.py
+++ rubicon_project/rubicon_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# this "Player" model class links one-to-one with a user (for adding fields to users)
+class Player(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    def __str__(self):
+        return '%s: %s %s' % ( self.user.username, self.user.first_name, self.user.last_name )
